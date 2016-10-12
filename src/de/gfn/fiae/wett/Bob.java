bfeec5261a7c3822872ebdcc8c3406c2c6c62a6e
RAGE <User@10.6.11.5>
2016-10-12 09:11:46 +0200

    Bob.java geämdert

--- src/de/gfn/fiae/wett/Bob.java
+++ src/de/gfn/fiae/wett/Bob.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.gfn.fiae.wett;
 
-/**
- *
- * @author pit
- */
 public class Bob {
+    public static void main(String[] args) {
+        System.out.println("Hallo!");
+    }
     
 }
