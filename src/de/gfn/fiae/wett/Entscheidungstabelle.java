bf67b738eb3f16cdb14fedecc8ac8fc01230988c
RAGE <User@10.6.11.5>
2016-10-12 09:24:27 +0200

    Entscheidungstabelle.java -> switch mit if ersetzt

--- src/de/gfn/fiae/wett/Entscheidungstabelle.java
+++ src/de/gfn/fiae/wett/Entscheidungstabelle.java
@@ -1,26 +1,25 @@
 package de.gfn.fiae.wett;
 
 public class Entscheidungstabelle {
+
     private String ausgabe_und;
     private String ausgabe_oder;
-    
-    public String undWert (String eingabe_a, String eingabe_b) {
+
+    public String undWert(String eingabe_a, String eingabe_b) {
         String eingabe = eingabe_a + "" + eingabe_b;
-        if(eingabe == "11") {
+        if ("11".equals(eingabe)) {
             ausgabe_und = "Wahr";
-        }
-        else {
+        } else {
             ausgabe_und = "Falsch";
         }
         return ausgabe_und;
     }
-    
-    public String oderWert (String eingabe_a, String eingabe_b) {
+
+    public String oderWert(String eingabe_a, String eingabe_b) {
         String eingabe = eingabe_a + "" + eingabe_b;
-        if(eingabe == "00") {
+        if ("00".equals(eingabe)) {
             ausgabe_oder = "Falsch";
-        }
-        else {
+        } else {
             ausgabe_oder = "Wahr";
         }
         return ausgabe_oder;
